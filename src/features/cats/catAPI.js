a2eef4ac7e95e800a73ae4994d1b37c4b809cd56
Jeffrey Naval <epsilogs@gmail.com>
2021-12-14 12:30:04 +0800

    axios cache adapter

--- src/features/cats/catAPI.js
+++ src/features/cats/catAPI.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { setupCache } from 'axios-cache-adapter';
 
 /**
  * The API Key provided by thecatapi.
@@ -7,13 +8,23 @@ import axios from 'axios'
 const apiKey = '89c72587-01b5-49c0-93ca-263055fb166e';
 
 /**
+ * Axios cache adapter. It will increase page
+ * performance and user experience.
+ */
+const cacheInMinutes = 15;
+const cache = setupCache({
+  maxAge: cacheInMinutes * 60 * 1000
+});
+
+/**
  * The axios instance that we will
  * use to call the cat api using
  * the apiKey
  */
 const catAPI = axios.create({
   baseURL: 'https://api.thecatapi.com/v1',
-  headers: { 'x-api-key': apiKey }
+  headers: { 'x-api-key': apiKey },
+  adapter: cache.adapter
 });
 
 export default catAPI;
