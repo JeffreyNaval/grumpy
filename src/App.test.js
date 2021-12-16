62942a8c3277d57ab7d175966c6c27bafd4a5e78
Jeffrey Naval <epsilogs@gmail.com>
2021-12-16 12:02:47 +0800

    testing package

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,18 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-test('renders learn react link', () => {
+test('renders meowstagram', () => {
   const { getByText } = render(
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}/>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/Meowstagram/i)).toBeInTheDocument();
 });
